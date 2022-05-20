5a774e1e3137656e71061694d99d5481f8ee67c8
0xjsthon <torikussadik404@gmail.com>
2022-05-21 00:55:44 +0600

    0,0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import resturant from './img/resturant.jpg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Main adj="amazing" dishes={dishobj} />
+      <Footer year={new Date().getFullYear()} />
     </div>
   );
+}
+
+
+// --> Header 
+
+function Header(props) {
+  return(
+    <header>
+      <h1>
+        Horacio's kitchen
+      </h1>
+    </header>
+  )
+}
+
+
+// --> Main 
+
+function Main(props) {
+  let rad = {listStyle: "none"}
+  return( 
+    <section>
+      <p>We serve the most {props.adj} food around.</p>
+      <img src={resturant} style={rad} alt="" height={250}/>
+      <ul>
+        {props.dishes.map((dish) => <li style={rad} key={dish.id}> {dish.title} </li> )}
+      </ul>
+    </section>
+  )
+}
+
+const dishes = [
+  'Macaroni and Cheese',
+  'Salmon',
+  'Tofu with vegetables',
+  'Minestrone'
+]
+
+const dishobj = dishes.map((dish, i) => ({id: i, title: dish}))
+
+
+
+
+// --> Footer 
+
+function Footer(props) {
+  
+  return( 
+    <footer>
+      <p>Copyright {props.year}</p>
+    </footer>
+  )
 }
 
 export default App;
